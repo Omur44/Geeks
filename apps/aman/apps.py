ee65ca0014428458f232af51e3730798393eec1b
Omur44 <maksutovomurbek680@gmail.com>
2026-02-11 17:48:02 +0600

    2 dz

--- apps/aman/apps.py
+++ apps/aman/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AmanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.aman'
+    verbose_name = "Основные настройки"
--- apps/aman/models.py
+++ apps/aman/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Settings(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название сайта')
+    logo = models.ImageField(upload_to='logo', verbose_name='Логотип')
+    icon = models.ImageField(upload_to='icon', verbose_name='Иконка')
+    descriptions = models.TextField(verbose_name='Описание сайта')
+    phone_number = models.CharField(max_length=25, verbose_name='Номер телефона')
+    email = models.EmailField(verbose_name='Электронная почта')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
+    locate = models.URLField(verbose_name='Ссылка на карту')
+
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Настройки'
+        verbose_name_plural = 'Настройки'
+
 # Create your models here.
